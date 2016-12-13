14a2be0f6d815f8f9d30d0d33c5cb5405a7ee819
Markus Tacker <m@coderbyheart.com>
2016-12-13 17:13:34 +0100

    chore(deploy): only deploy tags.

--- config.web.js
+++ config.web.js
@@ -1,8 +1,10 @@
 'use strict'
 
 import config from './config'
+import pjson from './package.json'
 
 module.exports = {
   environment: config.get('environment'),
-  basePath: config.get('base_path')
+  basePath: config.get('base_path'),
+  version: pjson.version
 }
